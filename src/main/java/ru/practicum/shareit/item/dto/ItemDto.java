f52a6d7dd22bc78e72dfda61b55c28a374491ea7
Valerii <Kovaleff615@yandex.ru>
2024-08-30 14:32:16 +0300

    описал модели данных для пользователя(Dto)

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String available; // — статус о том, доступна или нет вещь для аренды;
+    private Long owner; // — владелец вещи;
+    private String request; /*  — если вещь была создана по запросу другого пользователя,
+                                то в этом поле будет храниться ссылка на соответствующий запрос.*/
 }
