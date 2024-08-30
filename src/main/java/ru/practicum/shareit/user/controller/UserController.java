e26d990e413844084a7c6b8af4dc1ffdad1c1ad1
Valerii <Kovaleff615@yandex.ru>
2024-08-30 16:51:26 +0300

    добавил эндпоинты на добавление, получение, удаление и обновление пользователя

---
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -0,0 +1,40 @@
+package ru.practicum.shareit.user.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserDto addUser(@Valid @RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+}
