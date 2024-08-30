db11489205b20e90353e8555e4f716618342f28d
Valerii <Kovaleff615@yandex.ru>
2024-08-30 14:33:02 +0300

    добавил мапперы для преобразования из dal в dto

---
+++ src/main/java/ru/practicum/shareit/item/mappers/ItemRowMapper.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.item.mappers;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+@Component
+public class ItemRowMapper {
+    public static ItemDto toItemDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .owner(item.getOwner())
+                .request(item.getRequest())
+                .build();
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/user/mappers/UserRowMapper.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.user.mappers;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+@Component
+public class UserRowMapper {
+    public UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+}
