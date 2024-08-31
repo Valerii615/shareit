d432253e7a68dfa4e814cb1210c65ef44b16647d
Valerii <Kovaleff615@yandex.ru>
2024-08-31 11:00:30 +0300

    добавил логирование

--- src/main/java/ru/practicum/shareit/user/repository/InMemoryUserStorage.java
+++ src/main/java/ru/practicum/shareit/user/repository/InMemoryUserStorage.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.user.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.exception.ConflictException;
 import ru.practicum.shareit.exception.NotFoundException;
@@ -10,6 +11,7 @@ import ru.practicum.shareit.user.model.User;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Repository
 public class InMemoryUserStorage {
     private final UserRowMapper userRowMapper = new UserRowMapper();
@@ -18,30 +20,39 @@ public class InMemoryUserStorage {
     private Long countId = 0L;
 
     public UserDto addUser(UserDto userDto) {
+        log.info("Adding new user: {}", userDto);
         validateUserEmail(userDto.getEmail());
         User user = User.builder()
-                .id(getCountId())
+                .id(++countId)
                 .name(userDto.getName())
                 .email(userDto.getEmail())
                 .build();
         users.put(user.getId(), user);
-        return userRowMapper.toUserDto(users.get(user.getId()));
+        UserDto userDto1 = userRowMapper.toUserDto(users.get(user.getId()));
+        log.info("Added user: {}", userDto1);
+        return userDto1;
     }
 
     public UserDto getUser(Long id) {
+        log.info("Getting user id: {}", id);
         User user = users.get(id);
         if (user == null) {
             throw new NotFoundException("Пользователь с id: " + id + " не найден");
         }
-        return userRowMapper.toUserDto(user);
+        UserDto userDto = userRowMapper.toUserDto(user);
+        log.info("Found user: {}", userDto);
+        return userDto;
     }
 
     public void deleteUser(Long id) {
+        log.info("Deleting user id: {}", id);
         getUser(id);
         users.remove(id);
+        log.info("Deleted user id: {}", id);
     }
 
     public UserDto updateUser(Long id, UserDto userDto) {
+        log.info("Updating user id: {}, user: {}", id, userDto.toString());
         getUser(id);
         User user = users.get(id);
         user.setId(id);
@@ -53,11 +64,9 @@ public class InMemoryUserStorage {
             user.setEmail(userDto.getEmail());
         }
         users.put(id, user);
-        return userRowMapper.toUserDto(users.get(id));
-    }
-
-    public Long getCountId() {
-        return ++countId;
+        UserDto userDto1 = userRowMapper.toUserDto(users.get(id));
+        log.info("Updated user: {}", userDto1);
+        return userDto1;
     }
 
     public void validateUserEmail(String email) {
