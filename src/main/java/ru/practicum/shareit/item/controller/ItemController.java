62cea4e68dfd49c90dce67e1d22225e58e6eaee7
Valerii <Kovaleff615@yandex.ru>
2024-08-31 10:39:33 +0300

    описал эндпоинты контроллера для работы с вещами согласно тз

---
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -0,0 +1,50 @@
+package ru.practicum.shareit.item.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+
+import java.util.List;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping
+    public ItemDto addItem(@RequestHeader("X-Sharer-User-Id") Long id, @Valid @RequestBody ItemDto itemDto) {
+        return itemService.addItem(id, itemDto);
+    }
+
+    @GetMapping("/{id}")
+    public ItemDto getItem(@PathVariable Long id) {
+        return itemService.getItem(id);
+    }
+
+    @PatchMapping("/{id}")
+    public ItemDto updateItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+                              @PathVariable Long id,
+                              @RequestBody ItemDto itemDto) {
+        return itemService.updateItem(userId, id, itemDto);
+    }
+
+    @GetMapping
+    public List<ItemDto> getAllItemsOfUser(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return itemService.getAllItemsOfUser(userId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItemsForRental(@RequestParam(value = "text") String text) {
+        return itemService.searchItemsForRental(text);
+    }
+
+
+}
