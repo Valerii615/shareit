12a243f1f4016c8a7f9c2e13a887be407cf61113
Valerii <Kovaleff615@yandex.ru>
2024-08-31 10:40:51 +0300

    поправил код маппера

--- src/main/java/ru/practicum/shareit/item/mappers/ItemRowMapper.java
+++ src/main/java/ru/practicum/shareit/item/mappers/ItemRowMapper.java
@@ -6,13 +6,12 @@ import ru.practicum.shareit.item.model.Item;
 
 @Component
 public class ItemRowMapper {
-    public static ItemDto toItemDto(Item item) {
+    public ItemDto toItemDto(Item item) {
         return ItemDto.builder()
-//                .id(item.getId())
+                .id(item.getId())
                 .name(item.getName())
                 .description(item.getDescription())
                 .available(item.getAvailable())
-//                .owner(item.getOwner())
                 .request(item.getRequest())
                 .build();
     }
