a7e7375219cba8dfe35753e32b392e267bcc4f6a
Valerii <Kovaleff615@yandex.ru>
2024-08-31 10:40:14 +0300

    добавил валидацию необходимых полей

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ItemDto {
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
+    @NotNull
     private Boolean available; // — статус о том, доступна или нет вещь для аренды;
-    private Long owner; // — владелец вещи;
     private Long request; /*  — если вещь была создана по запросу другого пользователя,
                                 то в этом поле будет храниться ссылка на соответствующий запрос.*/
 }
