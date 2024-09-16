21dbabee7dbe03a80a54689303592c58c928f70f
Valerii <Kovaleff615@yandex.ru>
2024-09-16 14:30:06 +0300

    изменил работу методов с InMemoryUserStorage на UserDbStorage

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,36 +1,48 @@
 package ru.practicum.shareit.user.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.repository.UserDbStorage;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.mappers.UserMapper;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.repository.InMemoryUserStorage;
 
 @Service
-public class UserService {
-    private final InMemoryUserStorage inMemoryUserStorage;
+@RequiredArgsConstructor
+public class UserService implements UserServiceInterface {
+    private final UserDbStorage userDbStorage;
     private final UserMapper userMapper;
 
-    public UserService(InMemoryUserStorage inMemoryUserStorage, UserMapper userMapper) {
-        this.inMemoryUserStorage = inMemoryUserStorage;
-        this.userMapper = userMapper;
-    }
-
-    public UserDto addUser(UserDto userDto) {
+    @Override
+    public UserDto createUser(UserDto userDto) {
         User user = userMapper.toUser(userDto);
-        return userMapper.toUserDto(inMemoryUserStorage.addUser(user));
+        return userMapper.toUserDto(userDbStorage.save(user));
     }
 
-    public UserDto getUser(Long id) {
-        return userMapper.toUserDto(inMemoryUserStorage.getUser(id));
-    }
-
-    public void deleteUser(Long id) {
-        inMemoryUserStorage.deleteUser(id);
-    }
-
+    @Override
     public UserDto updateUser(Long id, UserDto userDto) {
-        User user = userMapper.toUser(userDto);
-        return userMapper.toUserDto(inMemoryUserStorage.updateUser(id, user));
+        User newUser = userMapper.toUser(userDto);
+        User user = userDbStorage.findById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id: " + id + " не найден"));
+        newUser.setId(id);
+        if (newUser.getName() == null) {
+            newUser.setName(user.getName());
+        }
+        if (newUser.getEmail() == null) {
+            newUser.setEmail(user.getEmail());
+        }
+        return userMapper.toUserDto(userDbStorage.save(newUser));
+    }
+
+    @Override
+    public UserDto findUserById(Long id) {
+        return userMapper.toUserDto(userDbStorage.findById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id: " + id + " не найден")));
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        userDbStorage.deleteById(id);
     }
 }
