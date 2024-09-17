c9701f6790240f46f903fe25d1e5497f0a124820
Valerii <Kovaleff615@yandex.ru>
2024-09-17 16:38:09 +0300

    добавил реализовал эндпоинты согласно тз

---
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.booking.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.Service.BookingService;
+import ru.practicum.shareit.booking.Service.BookingServiceImpl;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingDtoRequest;
+import ru.practicum.shareit.booking.model.Status;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+    private final BookingService bookingServiceImpl;
+
+    @PostMapping
+    public BookingDto addBooking(@RequestHeader("X-Sharer-User-Id") Long userId, @Valid @RequestBody BookingDtoRequest bookingDtoRequest) {
+        return bookingServiceImpl.createBooking(userId, bookingDtoRequest);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingDto updateBookingApprove(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                           @PathVariable Long bookingId,
+                                           @RequestParam(value = "approved") boolean approved) {
+        return bookingServiceImpl.updateBookingApprove(userId, bookingId, approved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public BookingDto getBookingById(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long bookingId) {
+        return bookingServiceImpl.getBookingDtoById(userId, bookingId);
+    }
+
+    @GetMapping
+    public List<BookingDto> getAllBookingsFromUser(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                 @RequestParam(value = "state", defaultValue = "ALL") Status state) {
+        return bookingServiceImpl.getAllBookingsFromUser(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingDto> getAllBookingFromOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                   @RequestParam(value = "state", defaultValue = "ALL") Status state) {
+        return bookingServiceImpl.getAllBookingFromOwner(userId, state);
+    }
+}
