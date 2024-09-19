c04f0665647962320fa93245bb9c0b3acd0202ef
Valerii <Kovaleff615@yandex.ru>
2024-09-19 09:31:18 +0300

    Code-style и добавление новых методов доступа к базе данных

--- src/main/java/ru/practicum/shareit/booking/repository/BookingDbStorage.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingDbStorage.java
@@ -4,11 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.Status;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingDbStorage extends JpaRepository<Booking, Long> {
     List<Booking> findByBookerIdOrderByStart(Long bookerId);
+
     List<Booking> findByBookerIdAndStatusOrderByStart(Long bookerId, Status status);
+
     List<Booking> findByItemOwnerIdOrderByStart(Long ownerId);
+
     List<Booking> findByItemOwnerIdAndStatusOrderByStart(Long ownerId, Status status);
+
+    Booking findByItemIdAndStartBeforeOrderByStart(Long itemId, LocalDateTime timeNow);
+
+    Booking findByItemIdAndStartAfterOrderByStart(Long itemId, LocalDateTime timeNow);
+
+    List<Booking> findByItemIdAndBookerIdAndEndBefore(Long itemId, Long bookerId, LocalDateTime timeNow);
 }
