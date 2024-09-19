b33c84cd041bd27093c1c9d388f40a9ce16acad4
Valerii <Kovaleff615@yandex.ru>
2024-09-19 09:37:49 +0300

    добавил описание сущности комментарий

---
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit.item.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "comment_text")
+    private String text;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+}
