887856f0361bb3469f3e0d19b7244c7d40da1eec
Valerii <Kovaleff615@yandex.ru>
2024-09-19 09:33:35 +0300

    добавил маппинг новой сушности

--- src/main/java/ru/practicum/shareit/item/mappers/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mappers/ItemMapper.java
@@ -2,12 +2,24 @@ package ru.practicum.shareit.item.mappers;
 
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoTime;
 import ru.practicum.shareit.item.model.Item;
 
 @Component
 public class ItemMapper {
+
     public ItemDto toItemDto(Item item) {
         return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .request(item.getRequest())
+                .build();
+    }
+
+    public ItemDtoTime toItemDtoTime(Item item) {
+        return ItemDtoTime.builder()
                 .id(item.getId())
                 .name(item.getName())
                 .description(item.getDescription())
