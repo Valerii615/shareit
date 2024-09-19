76e5b714b4e542078e3f2f18958c47943212df6e
Valerii <Kovaleff615@yandex.ru>
2024-09-19 09:32:57 +0300

    добавил эндпоинт для работы с комментариями

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -2,7 +2,10 @@ package ru.practicum.shareit.item.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.CommentText;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoTime;
 import ru.practicum.shareit.item.service.ItemServiceImpl;
 
 import java.util.List;
@@ -21,9 +24,16 @@ public class ItemController {
         return itemService.createItem(id, itemDto);
     }
 
+    @PostMapping("/{itemId}/comment")
+    public CommentDto addComment(@RequestHeader("X-Sharer-User-Id") Long id,
+                                 @PathVariable Long itemId,
+                                 @RequestBody CommentText commentText) {
+        return itemService.addComment(id, itemId, commentText);
+    }
+
     @GetMapping("/{id}")
-    public ItemDto getItem(@PathVariable Long id) {
-        return itemService.findItemDtoById(id);
+    public ItemDtoTime getItem(@PathVariable Long id) {
+        return itemService.findItemDtoTimeById(id);
     }
 
     @PatchMapping("/{id}")
