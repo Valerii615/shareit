9f30722bf1cd5c95f3ed1400ad801309391d9260
Valerii <Kovaleff615@yandex.ru>
2024-10-07 12:14:02 +0300

    добавил тесты

--- gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -13,11 +13,13 @@ import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = BookingController.class)
-public class BookingClientTest {
+public class BookingControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -28,6 +30,10 @@ public class BookingClientTest {
     private ObjectMapper objectMapper;
 
     private static final String API_PREFIX = "/bookings";
+    private static final String API_PREFIX_BAD_STATE = "/bookings?state=bd";
+    private static final String API_PREFIX_BAD_FROM = "/bookings?state=all&from=a";
+    private static final String API_PREFIX_NEGATIVE_FROM = "/bookings?state=all&from=-1";
+    private static final String API_PREFIX_BAD_SIZE = "/bookings?state=all&from=0&size=0";
     private static final Long USER_ID = 1L;
 
     @Test
@@ -79,6 +85,64 @@ public class BookingClientTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void bookItemBadState() throws Exception {
+        try {
+            mockMvc.perform(get(API_PREFIX_BAD_STATE)
+                            .header("X-Sharer-User-Id", USER_ID)
+                            .characterEncoding(StandardCharsets.UTF_8)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+        } catch (Exception e) {
+            assertEquals("Request processing failed: java.lang.IllegalArgumentException: Unknown state: bd", e.getMessage(), "Получено неверное исключение");
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    void bookItemBadFrom() throws Exception {
+        mockMvc.perform(get(API_PREFIX_BAD_FROM)
+                        .header("X-Sharer-User-Id", USER_ID)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    @SneakyThrows
+    void bookItemNegativeFrom() throws Exception {
+        try {
+            mockMvc.perform(get(API_PREFIX_NEGATIVE_FROM)
+                            .header("X-Sharer-User-Id", USER_ID)
+                            .characterEncoding(StandardCharsets.UTF_8)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+        } catch (Exception e) {
+            assertEquals("Request processing failed: jakarta.validation.ConstraintViolationException: getBookings.from: must be greater than or equal to 0", e.getMessage(), "Получено неверное исключение");
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    void bookItemBadSize() throws Exception {
+        try {
+            mockMvc.perform(get(API_PREFIX_BAD_SIZE)
+                            .header("X-Sharer-User-Id", USER_ID)
+                            .characterEncoding(StandardCharsets.UTF_8)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+        } catch (Exception e) {
+            assertEquals("Request processing failed: jakarta.validation.ConstraintViolationException: getBookings.size: must be greater than 0", e.getMessage(), "Получено неверное исключение");
+        }
+
     }
 
 }
